351b8c310f12d2eed7843c8e8352a86d2377d1f2
LullabyOfLies <jeanlorenz.recato@tup.edu.ph>
2023-05-23 10:59:39 +0800

    fixed intro and footer links

--- rebuilt/NassiPy/components/footer.js
+++ rebuilt/NassiPy/components/footer.js
@@ -6,11 +6,10 @@ class Footer extends HTMLElement{
                     <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12">
                             <ul class="social">
-                                <li><a href="https://www.facebook.com/" target="_blank" rel="noopener"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="https:/www.twitter.com" target="_blank" rel="noopener"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="https:/www.linkedin.com" target="_blank" rel="noopener"><i class="fa fa-linkedin"></i></a></li>
-                                <li><a href="#" target="_blank" rel="noopener"><i class="fa fa-rss"></i></a></li>
-                                <li><a href="https:/www.dribbble.com" target="_blank" rel="noopener"><i class="fa fa-dribbble"></i></a></li>
+                                <li><a href="https://github.com/LullabyOfLies" target="_blank" rel="noopener"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                                <li><a href="https://www.youtube.com/@nassipy23" target="_blank" rel="noopener"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                                <li><a href="https://www.linkedin.com/in/cristian-bien-armedilla-559a34268/" target="_blank" rel="noopener"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="https://www.facebook.com/LesterJosephGloria" target="_blank" rel="noopener"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
                             </ul>
                         </div>  
                     </div>
